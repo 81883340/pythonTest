76919a6d6f2100fce610c1ce7168d6bffc9aab86
81883340 <45990096+81883340@users.noreply.github.com>
2024-12-20 17:26:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ custom_objects_data = [
 def login_to_salesforce(token):
     try:
         # 使用提供的token作为OAuth Token来登录到Salesforce
-        sf = Salesforce(instance_url='login.salesforce.com', session_id=token)
+        sf = Salesforce(instance_url='https://ibm176-dev-ed.develop.my.salesforce.com', session_id=token)
         return sf
     except SalesforceAuthenticationFailed as e:
         print(f"Salesforce Authentication Failed: {e}")
